f30f2f81bc9e02ba2563b7d5ac628927da596d52
Chlunidia Putri Kieswendha <chlunidia.putri.ft21@mail.umy.ac.id>
2022-12-30 17:42:34 +0700

    Create method postorder

--- BinarySearchTree/Program.cs
+++ BinarySearchTree/Program.cs
@@ -99,7 +99,21 @@ namespace BinarySearchTree
                 preorder(ptr.leftchild);
                 preorder(ptr.rightchild);
             }
-        }       
+        }
+        public void postorder(Node ptr) /*Performs the postorder traversal of the tree*/
+        {
+            if (ROOT == null)
+            {
+                Console.WriteLine("Tree is empty");
+                return;
+            }
+            if (ptr != null)
+            {
+                postorder(ptr.leftchild);
+                postorder(ptr.rightchild);
+                Console.Write(ptr.info + " ");
+            }
+        }
         static void Main(string[] args)
         {
         }
