affe6780b9eeaaa3de49cc6733f48c9f8ef1acf6
Chlunidia Putri Kieswendha <chlunidia.putri.ft21@mail.umy.ac.id>
2022-12-30 17:27:30 +0700

    Create class BinaryTree, variable ROOT, initializing ROOT to null, and create method insert

--- BinarySearchTree/Program.cs
+++ BinarySearchTree/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.CodeDom.Compiler;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,7 +21,22 @@ namespace BinarySearchTree
             rightchild = r;
         }
     }
-   
+
+    /*A node class consist of three things, the information, reference to the right child, and reference to the left child*/
+
+    class BinaryTree
+    {
+        public Node ROOT;
+        public BinaryTree()
+        {
+            ROOT = null; /*Initializing ROOT to null*/
+        }
+        public void insert(string element) /*Insert a node in the binary search tree*/
+        {
+            Node tmp, parent = null, currentNode = null;
+            Search(element, ref parent, ref currentNode);           
+        }
+    }
     internal class Program
     {
         static void Main(string[] args)
