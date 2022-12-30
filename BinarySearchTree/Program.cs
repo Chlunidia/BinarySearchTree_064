b042c64474e8f42cc29d02239de8dad729f33475
Chlunidia Putri Kieswendha <chlunidia.putri.ft21@mail.umy.ac.id>
2022-12-30 17:16:53 +0700

    Create class Node and create variable info, leftchild, and rightchild

--- BinarySearchTree/Program.cs
+++ BinarySearchTree/Program.cs
@@ -6,6 +6,14 @@ using System.Threading.Tasks;
 
 namespace BinarySearchTree
 {
+    class Node
+    {
+        public string info;
+        public Node leftchild;
+        public Node rightchild;
+       
+    }
+   
     internal class Program
     {
         static void Main(string[] args)
